ef36b027a34ae276917085f4b815998652511a3f
Melike Yılmaz <deneme@gmail.com>
2024-01-18 14:07:07 +0300

    Add project files.

---
+++ GidaFirmaKampanyaKodUygulamasi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+       
+        List<string> generatedCodes = GenerateCodes(1000);
+
+        foreach (var code in generatedCodes)
+        {
+            bool isValid = CheckCode(code);
+            Console.WriteLine($"Code: {code}, IsValid: {isValid}");
+        }
+    }
+
+    static List<string> GenerateCodes(int count)
+    {
+        List<string> codes = new List<string>();
+        Random random = new Random();
+
+        while (codes.Count < count)
+        {
+          
+            string code = new string(Enumerable.Repeat("ACDEFGHKLMNPRTXYZ234579", 8)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+           
+            if (!codes.Contains(code))
+            {
+                codes.Add(code);
+            }
+        }
+
+        return codes;
+    }
+
+    static bool CheckCode(string code)
+    {
+        
+        string pattern = "^[ACDEFGHKLMNPRTXYZ234579]{8}$";
+        return Regex.IsMatch(code, pattern);
+    }
+}
